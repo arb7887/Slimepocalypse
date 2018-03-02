0f36d18c5a10116e9f584775e2adf0317503dc49
HanSoo Lee <hxl9400@rit.edu>
2018-03-01 23:50:55 -0500

    Slime spawning

---
+++ Assets/Scripts/SlimeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeManager : MonoBehaviour {
+
+    public GameObject slimePrefab;
+    public List<GameObject> slimeList;
+    float timer;
+
+    // Use this for initialization
+    void Start () {
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 1.0f)
+        {
+            int random = Random.Range(1, 8);
+            GameObject newSlime = Instantiate(slimePrefab);
+            switch (random)
+            { 
+                case (1):
+                    newSlime.transform.position = new Vector3(-10.0f, 3.6f, 0.0f);
+                    break;
+                case (2):
+                    newSlime.transform.position = new Vector3(-7.5f, 3.6f, 0.0f);
+                    break;
+                case (3):
+                    newSlime.transform.position = new Vector3(-5.0f, 3.6f, 0.0f);
+                    break;
+                case (4):
+                    newSlime.transform.position = new Vector3(-2.5f, 3.6f, 0.0f);
+                    break;
+                case (5):
+                    newSlime.transform.position = new Vector3(2.5f, 3.6f, 0.0f);
+                    break;
+                case (6):
+                    newSlime.transform.position = new Vector3(5.0f, 3.6f, 0.0f);
+                    break;
+                case (7):
+                    newSlime.transform.position = new Vector3(7.5f, 3.6f, 0.0f);
+                    break;
+                case (8):
+                    newSlime.transform.position = new Vector3(10.0f, 3.6f, 0.0f);
+                    break;
+            }
+            slimeList.Add(newSlime);
+            timer = 0.0f;
+        }
+	}
+}
