5db6b2e09384815826d66e32eaa9b1a74a1bfa42
Steven Ma <sxm7686@rit.edu>
2018-03-28 01:01:10 -0400

    Added in the Main Menu and associated functionality.

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -23,4 +23,13 @@ public class GameOverMenu : MonoBehaviour {
         KillCounter.instance.SetKillCount(0);
         SceneManager.LoadScene("Main");
     }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        KillCounter.instance.SetScore(0);
+        KillCounter.instance.SetTimer(0.0f);
+        KillCounter.instance.SetKillCount(0);
+        SceneManager.LoadScene("MainMenu");
+    }
 }
